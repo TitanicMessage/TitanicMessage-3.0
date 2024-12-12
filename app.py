f949ccfbd53362810b2439e1727d930f76d4f38d
uukelele-scratch <robustrobot11@gmail.com>
2024-12-12 22:35:59 +0000

    Added basic login/signup system

--- app.py
+++ app.py
@@ -8,5 +8,43 @@ import json
 app = Flask(__name__)
 app.url_map.strict_slashes = False # Making sure 'site/page' is equal to 'site/page/'
 
+def load_accounts():
+	with open('accounts.json', 'r') as f:
+		j = json.load(f)
+		return j
+
+def write_accounts(accounts):
+	with open('accounts.json', 'w') as f:
+		json.dump(accounts, f, indent=4)
+	return "OK"
+
+def create_account(username, password):
+	salt = bcrypt.gensalt()
+	hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+	accounts = load_accounts()
+	accounts[username] = {}
+	accounts[username]['password_hashed'] = hashed
+	return write_accounts(accounts)
+
+def check_login(username, password):
+	accounts = load_accounts()
+	if not username in accounts:
+		return "Account not found"
+	return bcrypt.checkpw(password.encode('utf-8'), accounts[username]['password'].encode('utf-8'))
+
+@app.route('/endpoint/create_account', methods=['POST'])
+def create_account():
+	j = request.get_json()
+	username = j['username']
+	password = j['password']
+	return create_account(username, password)
+
+@app.route('/endpoint/login', methods=['POST'])
+def verify_login():
+	j = request.get_json()
+	username = j['username']
+	password = j['password']
+	return check_login(username, password)
+
 if __name__ == '__main__':
 	app.run(debug=True, port='8080', host='0.0.0.0')
